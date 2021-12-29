9dc8064919922009c80599e5d466666197ae88ef
Ayuan Zhao <ayuan@Ayuans-Mac.local>
2021-12-29 12:28:53 -0800

    fibo

---
+++ src/main/java/ayuan/datastructure/FibNacci.java
@@ -0,0 +1,14 @@
+package ayuan.datastructure;
+
+public class FibNacci {
+
+    public static void main(String[] args) {
+        System.out.println("fibo");
+        fibo(5);
+    }
+
+    private static void fibo(int n) {
+
+    }
+
+}
